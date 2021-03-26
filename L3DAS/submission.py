b1378598a8081b833980594a0cf2d123d0e1b1ab
eric <>
2021-03-26 16:44:14 +0100

    init submission

---
+++ L3DAS/submission.py
@@ -0,0 +1,17 @@
+import os, sys
+import numpy as np
+
+def validate_task1_submission(submission_folder):
+    '''
+    - Check if the folder contains all files to submit for task 1
+    - Check if all files have the correct format, shape and sample rate
+    '''
+    pass
+
+
+def validate_task2_submission(submission_folder):
+    '''
+    - Check if the folder contains all files to submit for task 1
+    - Check if all files have the correct format
+    '''
+    pass
