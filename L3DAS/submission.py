24a842cc364692e5fe06c933bc898ee012a34cdb
eric <>
2021-03-29 17:59:49 +0200

    added some utilities

--- L3DAS/submission.py
+++ L3DAS/submission.py
@@ -6,7 +6,9 @@ def validate_task1_submission(submission_folder):
     - Check if the folder contains all files to submit for task 1
     - Check if all files have the correct format, shape and sample rate
     '''
-    pass
+    #this is just a draft
+    target_sr = 16000 
+    contents = os.listdir(submission_folder)
 
 
 def validate_task2_submission(submission_folder):
