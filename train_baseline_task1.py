d8f0d3a77fb1f49e7740e12b91d6538fa83e88b7
eric <>
2021-04-01 18:50:27 +0200

    cleaning repo

--- train_baseline_task1.py
+++ train_baseline_task1.py
@@ -48,7 +48,7 @@ def evaluate(model, dataloader):
     model.eval()
     test_loss = 0.
     with tqdm(total=len(dataloader) // args.batch_size) as pbar, torch.no_grad():
-        for example_num, (x, target) in enumerate(val_data):
+        for example_num, (x, target) in enumerate(dataloader):
             x, target = dyn_pad(x, target)
             target = target.to(device)
             x = x.to(device)
