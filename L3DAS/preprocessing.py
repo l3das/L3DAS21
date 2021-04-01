9ed1c542abe9f34ec1e33794968dd535b0860487
eric <>
2021-04-01 16:19:35 +0200

    task1 baseline

--- L3DAS/preprocessing.py
+++ L3DAS/preprocessing.py
@@ -14,8 +14,14 @@ def preprocessing_task1(args):
     sr_task1 = 16000
     def pad(x, size=sr_task1*10):
         #pad all sounds to 10 seconds
-        pad = np.zeros((x.shape[0], size))
-        pad[:,:x.shape[-1]] = x
+        length = x.shape[0]
+        if length > size:
+            print ('AHIAHIAHIAHIAHIAHIA', length)
+            pad = x[:,:size]
+        else:
+            pad = np.zeros((x.shape[0], size))
+            pad[:,:x.shape[-1]] = x
+
         return pad
 
     train100_folder = 'train'
