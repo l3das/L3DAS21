634e3f0370a0fae0c9eab50a2a2571c113377857
eric <>
2021-04-06 19:29:41 +0200

    task 1 adding fasnet

--- train_baseline_task1_fasnet.py
+++ train_baseline_task1_fasnet.py
@@ -250,53 +250,23 @@ if __name__ == '__main__':
     parser.add_argument('--use_cuda', type=str, default='True')
     parser.add_argument('--early_stopping', type=str, default='True')
     parser.add_argument('--fixed_seed', type=str, default='False')
-    parser.add_argument('--instruments', type=str, nargs='+', default=["vocals"],
-                        help="List of instruments to separate (default: \"vocals\")")
-    parser.add_argument('--num_workers', type=int, default=1,
-                        help='Number of data loader worker threads (default: 1)')
-    parser.add_argument('--features', type=int, default=32,
-                        help='Number of feature channels per layer')
-    parser.add_argument('--log_dir', type=str, default='RESULTS/waveunet/logs',
-                        help='Folder to write logs into')
-    parser.add_argument('--hdf_dir', type=str, default="hdf",
-                        help='Dataset path')
+
     parser.add_argument('--load_model', type=str, default=None,
                         help='Reload a previously trained model (whole task model)')
-    parser.add_argument('--lr', type=float, default=1e-3,
-                        help='Initial learning rate in LR cycle (default: 1e-3)')
-    parser.add_argument('--min_lr', type=float, default=5e-5,
-                        help='Minimum learning rate in LR cycle (default: 5e-5)')
-    parser.add_argument('--cycles', type=int, default=2,
-                        help='Number of LR cycles per epoch')
-    parser.add_argument('--batch_size', type=int, default=3,
+    parser.add_argument('--lr', type=float, default=0.00005,
+
+    parser.add_argument('--batch_size', type=int, default=20,
                         help="Batch size")
-    parser.add_argument('--levels', type=int, default=6,
-                        help="Number of DS/US blocks")
-    parser.add_argument('--depth', type=int, default=1,
-                        help="Number of convs per block")
+
     parser.add_argument('--sr', type=int, default=16000,
                         help="Sampling rate")
-    parser.add_argument('--channels', type=int, default=4,
-                        help="Number of input audio channels")
-    parser.add_argument('--kernel_size', type=int, default=5,
-                        help="Filter width of kernels. Has to be an odd number")
-    parser.add_argument('--output_size', type=float, default=1.0,
-                        help="Output duration")
-    parser.add_argument('--strides', type=int, default=4,
-                        help="Strides in Waveunet")
+
     parser.add_argument('--patience', type=int, default=20,
                         help="Patience for early stopping on validation set")
 
     parser.add_argument('--loss', type=str, default="L2",
                         help="L1 or L2")
-    parser.add_argument('--conv_type', type=str, default="gn",
-                        help="Type of convolution (normal, BN-normalised, GN-normalised): normal/bn/gn")
-    parser.add_argument('--res', type=str, default="fixed",
-                        help="Resampling strategy: fixed sinc-based lowpass filtering or learned conv layer: fixed/learned")
-    parser.add_argument('--separate', type=int, default=1,
-                        help="Train separate model for each source (1) or only one (0)")
-    parser.add_argument('--feature_growth', type=str, default="double",
-                        help="How the features in each layer should grow, either (add) the initial number of features each time, or multiply by 2 (double)")
+
     args = parser.parse_args()
     #eval string args
     args.use_cuda = eval(args.use_cuda)
