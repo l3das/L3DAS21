30590b4aca31c87fc17bb87e2c4a738bcfa58556
eric <>
2021-04-07 14:50:19 +0200

    adding segmentation

--- train_baseline_task1.py
+++ train_baseline_task1.py
@@ -229,7 +229,7 @@ if __name__ == '__main__':
     parser.add_argument('--load_model', type=str, default=None,
                         help='Reload a previously trained model (whole task model)')
     parser.add_argument('--lr', type=float, default=0.00001)
-    parser.add_argument('--batch_size', type=int, default=20,
+    parser.add_argument('--batch_size', type=int, default=15,
                         help="Batch size")
     parser.add_argument('--sr', type=int, default=16000,
                         help="Sampling rate")
