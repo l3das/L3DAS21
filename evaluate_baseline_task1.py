f8b8be45f65f88e2e0f94cf71c7e63fe21b8968f
eric <>
2021-04-09 11:21:52 +0200

    fasnet eval new

--- evaluate_baseline_task1.py
+++ evaluate_baseline_task1.py
@@ -171,8 +171,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     #i/o parameters
-    parser.add_argument('--model_path', type=str, default='RESULTS/fasnet_fulltrain100_REAL/checkpoint')
-    parser.add_argument('--results_path', type=str, default='RESULTS/fasnet_fulltrain100_REAL/metrics')
+    parser.add_argument('--model_path', type=str, default='RESULTS/fasnet_fulltrain100_nspk1/checkpoint')
+    parser.add_argument('--results_path', type=str, default='RESULTS/fasnet_fulltrain100_nspk1/metrics_provvisorio')
     #dataset parameters
     parser.add_argument('--predictors_path', type=str, default='DATASETS/processed/task1_mini/task1_predictors_test.pkl')
     parser.add_argument('--target_path', type=str, default='DATASETS/processed/task1_mini/task1_target_test.pkl')
