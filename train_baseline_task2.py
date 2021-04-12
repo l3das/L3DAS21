1e7bbb22713c2e7f5d5a5bdb19ac5162986e01fd
ericguizzo <eric.guizzo@hotmail.it>
2021-04-12 15:26:41 +0200

    baseline task1

--- train_baseline_task2.py
+++ train_baseline_task2.py
@@ -152,9 +152,9 @@ def main(args):
         train_loss = 0.
         with tqdm(total=len(tr_dataset) // args.batch_size) as pbar:
             for example_num, (x, target) in enumerate(tr_data):
-                print ('PRIMA', target.size)
+                print ('PRIMA', target.shape)
                 target = torch.flatten(target, start_dim=1)
-                print ('DOPO', target.size)
+                print ('DOPO', target.shape)
 
                 target = target.to(device)
                 x = x.to(device)
