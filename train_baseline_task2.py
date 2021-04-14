332a6fe49174cc9ba8cc06888a4dce82708a8078
ericguizzo <eric.guizzo@hotmail.it>
2021-04-14 20:32:35 +0200

    building seldnet

--- train_baseline_task2.py
+++ train_baseline_task2.py
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     parser.add_argument('--load_model', type=str, default=None,
                         help='Reload a previously trained model (whole task model)')
     parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--batch_size', type=int, default=15,
+    parser.add_argument('--batch_size', type=int, default=10,
                         help="Batch size")
     parser.add_argument('--sr', type=int, default=32000,
                         help="Sampling rate")
