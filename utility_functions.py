dd87bf48f6a35283a317ca7b4b6de56f6c2a3a50
ericguizzo <eric.guizzo@hotmail.it>
2021-04-14 17:38:52 +0200

    building seldnet

--- utility_functions.py
+++ utility_functions.py
@@ -106,6 +106,7 @@ def get_label_task2(path,frame_len,file_size,sample_rate,classes_,num_frames,
     the class ids of all sounds present in each data-point and
     their location coordinates, divided in 100 milliseconds frames.
     '''
+
     class_vec=[]
     loc_vec=[]
     num_classes=len(classes_)
@@ -177,7 +178,7 @@ def get_label_task2(path,frame_len,file_size,sample_rate,classes_,num_frames,
     loc_vec = np.array(loc_vec)
 
     loc_vec = loc_vec / max_label_distance  #normalize xyz (to use sigmoid in the model)
-    print ('aaa', np.max(class_vec))
+
     stacked = np.zeros((class_vec.shape[0],class_vec.shape[1]+loc_vec.shape[1]))
     stacked[:,:class_vec.shape[1]] = class_vec
     stacked[:,class_vec.shape[1]:] = loc_vec
