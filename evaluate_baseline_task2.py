c8a59317dc1d09cd320167082aff56d5a05cb716
ericguizzo <eric.guizzo@hotmail.it>
2021-04-15 16:55:38 +0200

    evaluation task 2

--- evaluate_baseline_task2.py
+++ evaluate_baseline_task2.py
@@ -51,12 +51,14 @@ def main(args):
 
     #LOAD MODEL
     if args.architecture == 'vgg16':
+        features_dim = int(test_target.shape[-2] * test_target.shape[-1])
         model = models.vgg16()
         model.features[0] = nn.Conv2d(args.input_channels, 64, kernel_size=(3, 3),
                                     stride=(1, 1), padding=(1, 1))
         model.classifier[6] =nn.Linear(in_features=4096,
                                     out_features=features_dim, bias=True)
     if args.architecture == 'vgg13':
+        features_dim = int(test_target.shape[-2] * test_target.shape[-1])
         model = models.vgg13()
         model.features[0] = nn.Conv2d(args.input_channels, 64, kernel_size=(3, 3),
                                     stride=(1, 1), padding=(1, 1))
