01cdabc7b67d53192c186a06a90e21c5a999ab0c
ericguizzo <eric.guizzo@hotmail.it>
2021-04-16 16:21:27 +0200

    added segmentation for task 2

--- train_baseline_task2.py
+++ train_baseline_task2.py
@@ -285,12 +285,12 @@ if __name__ == '__main__':
     parser.add_argument('--fixed_seed', type=str, default='False')
     parser.add_argument('--load_model', type=str, default=None,
                         help='Reload a previously trained model (whole task model)')
-    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--lr', type=float, default=0.00001)
     parser.add_argument('--batch_size', type=int, default=2,
                         help="Batch size")
     parser.add_argument('--sr', type=int, default=32000,
                         help="Sampling rate")
-    parser.add_argument('--patience', type=int, default=100,
+    parser.add_argument('--patience', type=int, default=300,
                         help="Patience for early stopping on validation set")
 
     #model parameters
